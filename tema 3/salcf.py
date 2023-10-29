b708cb32a45143179b475921bd5adc182762ab71
ioana pește <100304166+tbfidek@users.noreply.github.com>
2023-10-29 18:47:30 +0200

    fixed it :3

--- tema 3/salcf.py
+++ tema 3/salcf.py
@@ -1,23 +1,39 @@
-# deci basically maj functiilor sunt ok, e  o problema la update_domains_FC ptc gen cand face backtracking tre sa si dea revert la domeniile de dinainte cred 
-# dar am incercat sa fac copie si tot nu merge so at this point good luck and good night girlie finna go kms
+import copy
 
+# generates domains for each cell 
+def generate_domains(grid):
+    domains = []
+
+    for i in range(len(grid)):
+        row_domains = []
+        for j in range(len(grid[i])):
+            if grid[i][j] == 0:
+                row_domains.append([1, 2, 3, 4, 5, 6, 7, 8, 9])
+            elif grid[i][j] == -1:
+                row_domains.append([2, 4, 6, 8])
+            else:
+                row_domains.append([grid[i][j]])
+        domains.append(row_domains)
+
+    return domains
+
+
+# checks if the board is full
 def isComplete(board):
-    # Check if the Sudoku board is completely filled.
     for row in board:
         if 0 in row or -1 in row:
             return False
     return True
 
+
+# checks if the move is valid
 def possible(board, row, column, number):
-    # nr appears on given row
-    if number in board[row]:
+
+    # checks if number exists in the same row or column
+    if number in board[row] or number in [board[i][column] for i in range(9)]:
         return False
 
-    # nr appears on given column
-    if number in [board[i][column] for i in range(9)]:
-        return False
-
-    # nr appears on given subgrid
+    # checks if number exists in the same subgrid
     x0 = (column // 3) * 3
     y0 = (row // 3) * 3
     for i in range(3):
@@ -27,22 +43,24 @@ def possible(board, row, column, number):
 
     return True
 
+
+# updates the domains for all peers when a move is made
 def update_domains_FC(domains, row, col, value):
-   
-    # update the domain of the assigned variable (cell) to be only the assigned value.
+
+    # updates domain for current piece
     domains[row][col] = [value]
 
-    # Update the domains of peers in the same row.
+    # updates domain for peers in the same column
     for c in range(9):
         if c != col and value in domains[row][c]:
             domains[row][c].remove(value)
 
-    # Update the domains of peers in the same column.
+    # updates domain for peers in the same row 
     for r in range(9):
         if r != row and value in domains[r][col]:
             domains[r][col].remove(value)
 
-    # Update the domains of peers in the same 3x3 box.
+    # updates domain for peers in the same subgrid
     box_row, box_col = 3 * (row // 3), 3 * (col // 3)
     for r in range(box_row, box_row + 3):
         for c in range(box_col, box_col + 3):
@@ -51,19 +69,28 @@ def update_domains_FC(domains, row, col, value):
 
     return domains
 
+
+# finds the next piece which is a 0 or -1
 def find_unassigned_variables(board):
-    unassigned_variables = []  # List to store unassigned variables as (row, col) pairs.
+    unassigned_variables = []  # store unassigned variables as (row, col) pairs.
     for row in range(9):
         for col in range(9):
             if board[row][col] == 0 or board[row][col] == -1:
                 unassigned_variables.append((row, col))
     return unassigned_variables
 
-def BKT_with_FC(board, domains):
-    print()
-    print("i", board)
-    print()
-    print(domains)
+
+# returns row and col index for the next empty cell
+def next_unassigned_variable(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0 or board[row][col] == -1:
+                return row, col
+    return None, None
+
+
+# forward checking backtracking
+def fwd_checking(board, domains):
 
     if isComplete(board):
         return board
@@ -73,54 +100,40 @@ def BKT_with_FC(board, domains):
     if row is not None and col is not None:
         for value in domains[row][col]:
             if possible(board, row, col, value):
-                old_domain = domains[row][col]  #aici nu e bun (trb dat revert la toti peers cu domeniul vechi nu doar un element, am incercat asta si mi a luat foc calculatoru deci succes)
+                # keep the empty cell value for backtracking purposes
                 old_value = board[row][col]
+                # update the empty cell with a value
                 board[row][col] = value
-                updated_domains = update_domains_FC(domains, row, col, value)
+                # keep the domain values for backtracking purposes
+                domains_copy = copy.deepcopy(domains)
+                updated_domains = update_domains_FC(domains_copy, row, col, value)
+                # checks if any empty cells no longer have values in their domain => backtracking required
                 if not any(len(updated_domains[r][c]) == 0 for r, c in find_unassigned_variables(board)):
-                    result = BKT_with_FC(board, updated_domains.copy())
+                    result = fwd_checking(board, updated_domains)
                     if result:
                         return result
-                board[row][col] = old_value  # Backtrack
-                domains[row][col] = old_domain
-
+                board[row][col] = old_value  # backtrack if necessary
 
     return None
 
-def next_unassigned_variable(board):
-    for row in range(9):
-        for col in range(9):
-            if board[row][col] == 0 or board[row][col] == -1:
-                return row, col
-    return None, None
 
-# Initialize the Sudoku board and domains.
-grid = [[8, 4, 0, 0, 5, 0, -1, 0, 0],
-        [3, 0, 0, 6, 0, 8, 0, 4, 0],
-        [0, 0, -1, 4, 0, 9, 0, 0, -1],
-        [0, 2, 3, 0, -1, 0, 9, 8, 0],
-        [1, 0, 0, -1, 0, -1, 0, 0, 4],
-        [0, 9, 8, 0, -1, 0, 1, 6, 0],
-        [-1, 0, 0, 5, 0, 3, -1, 0, 0],
-        [0, 3, 0, 1, 0, 6, 0, 0, 7],
-        [0, 0, -1, 0, 2, 0, 0, 1, 3]]
+if __name__ == "__main__":
 
-domains = []
+    grid = [[8, 4, 0, 0, 5, 0, -1, 0, 0],
+            [3, 0, 0, 6, 0, 8, 0, 4, 0],
+            [0, 0, -1, 4, 0, 9, 0, 0, -1],
+            [0, 2, 3, 0, -1, 0, 9, 8, 0],
+            [1, 0, 0, -1, 0, -1, 0, 0, 4],
+            [0, 9, 8, 0, -1, 0, 1, 6, 0],
+            [-1, 0, 0, 5, 0, 3, -1, 0, 0],
+            [0, 3, 0, 1, 0, 6, 0, 0, 7],
+            [0, 0, -1, 0, 2, 0, 0, 1, 3]]
 
-for i in range(len(grid)):
-    row_domains = []
-    for j in range(len(grid[i])):
-        if grid[i][j] == 0:
-            row_domains.append([1,2,3,4,5,6,7,8,9])
-        elif grid[i][j] == -1:
-            row_domains.append([2,4,6,8])
-        else: 
-            row_domains.append([grid[i][j]])
-    domains.append(row_domains)
+    domains = generate_domains(grid)
+    result = fwd_checking(grid, domains)
 
-result = BKT_with_FC(grid, domains)
-if result:
-    for row in result:
-        print(row)
-else:
-    print("No solution found.")
+    if result:
+        for row in result:
+            print(row)
+    else:
+        print("no solution found")
