949ec018b186c52992674a4f994820d45031526e
daria <dariae9@yahoo.com>
2023-11-08 21:46:59 +0200

    updated minmax

--- tema 4/tema4.py
+++ tema 4/tema4.py
@@ -1,10 +1,12 @@
 import random
 
+
 def initialise_game():
     player1_moves = []
     player2_moves = []
     turn = 1
     return player1_moves, player2_moves, turn
+
 
 def final_state(state):
     if has_winning_combination(state[0]):
@@ -15,7 +17,8 @@ def final_state(state):
         return 'draw'
     else:
         return False
-    
+
+
 def has_winning_combination(moves):
     # check if there's a combination that sums up to 15
     for i in range(len(moves)):
@@ -25,12 +28,14 @@ def has_winning_combination(moves):
                     return True
     return False
 
+
 def valid_move(state, move):
     if move < 1 or move > 9:
         return False
-    if move in state[0] or move in state[1]:
+    if move in state[0] or move in state[1]: # checks to see if the number is already in the board
         return False  
     return True
+
 
 def transition(state, move):
     if not valid_move(state, move):
@@ -39,16 +44,18 @@ def transition(state, move):
     new_state[state[2] - 1].append(move)  # add the move to the current player's moves
     return new_state
 
+
 def choose_move(state):
     available_moves = [i for i in range(1, 10) if i not in state[0] and i not in state[1]]
     if state[2] == 1:
-        return random.choice(available_moves)
+        return random.choice(available_moves) # random move for player 1
     else:
         move = int(input("enter ur move: "))
         while not valid_move(state, move):
             move = int(input("invalid move, enter ur move: "))
         return move
-    
+
+
 def main():
     state = initialise_game()
     while not final_state(state):
@@ -58,6 +65,7 @@ def main():
 
     result = final_state(state)
     print(result)
+
 
 if __name__ == "__main__":
     main()
