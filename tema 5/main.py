20d5345bcaac8d68f2eaa394c142cd05479e197d
Ioana Peste <impeste@gmail.com>
2023-11-16 14:24:54 +0200

    cleanup

--- tema 5/main.py
+++ tema 5/main.py
@@ -2,61 +2,58 @@ from random import shuffle
 from network import Network
 import numpy as np
 
+
+# reads the dataset and splits it between attributes & labels 
 def read_data(filename):
     with open(filename, 'r') as fd:
+        # the attributes dataset
         data_input = []
+        # the labels dataset
         data_labels = []
         input_strings = []
         max_input_length = 0 
 
         for readline in fd:
-            string = readline.strip()
-            input_strings += [string]
-
-        shuffle(input_strings)
+            string = readline.strip() # removes spaces
+            input_strings += [string] # adds value to the list
+        
         shuffle(input_strings)
 
         labels_dict = {}
+   
         for string in input_strings:
-            input_arr = string.split('\t') 
-            input_values = list(map(lambda x: float(x) if x.strip() != '' else 0.0, input_arr[:-1]))
+            input_values = list(map(lambda x: float(x), string.split('\t')[:-1]))
+            # number of attributes (all columns, except the last one)
             max_input_length = max(max_input_length, len(input_values))
             data_input += [np.array(input_values)]
-            label_key = input_arr[-1]
+
+            # label for grain (last column)
+            label_key = string.split('\t')[-1]
             label_value = labels_dict.setdefault(label_key, len(labels_dict))
             data_labels += [label_value]
 
-        # zero-pad the input arrays to make them consistent in length
-        data_input = [np.pad(arr, (0, max_input_length - len(arr))) for arr in data_input]
+    return (np.array(data_input), np.array(data_labels)), max_input_length
 
-        inverted_dict = {}
-        for key, value in labels_dict.items():
-            inverted_dict[value] = key
-
-    return (np.array(data_input), np.array(data_labels)), inverted_dict
-
+# splits the data into training and testing sets
 def split_input_data(input_data):
     split_point = int(len(input_data[0]) * 8 / 10)
     train_set = (input_data[0][:split_point], input_data[1][:split_point])
     test_set = (input_data[0][split_point:], input_data[1][split_point:])
     return train_set, test_set
 
+# defines the number of neurons for each layer
 def init_layer_counts(input_data):
     input_layer_count = len(input_data[0][0]) # 7 neuroni pt 7 atribute
-    print(input_layer_count)
     output_layer_count = max(input_data[1]) + 1 # 3 neuroni pt 3 clase
     hidden_layer_count = int(2 / 3 * (input_layer_count + output_layer_count))
     return input_layer_count, hidden_layer_count, output_layer_count
 
-def print_set(data):
-    for i in range(len(data[0])):
-        print(data[0][i], data[1][i])
+def main():
+    input_data, _ = read_data('D:\\facultate\\an3\\ai\\tema 5\\seeds_dataset.txt') 
+    train_set, test_set = split_input_data(input_data)
+    network = Network(*init_layer_counts(input_data))
+    network.train(train_set, 0.001, 100)
+    (actual, predicted), results = network.test(test_set)
 
-input_data, labels_dict = read_data('D:\\facultate\\an3\\ai\\tema 5\\seeds_dataset.txt') 
-train_set, test_set = split_input_data(input_data)
-#print_set(train_set)
-network = Network(*init_layer_counts(input_data))
-network.train(train_set, 0.01, 10)
-(actual, predicted), results = network.test(test_set)
-
-#print(results)
+if __name__ == "__main__":
+    main()
--- tema 5/network.py
+++ tema 5/network.py
@@ -1,15 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Network():
-
+    # init network w/ specified layer sizes
     def __init__(self, input_layer_size, hidden_layer_size, output_layer_size):
+        # standard deviation
         hidden_layer_st_dev = 1 / np.sqrt(input_layer_size)
         output_layer_st_dev = 1 / np.sqrt(hidden_layer_size)
-        self.hidden_layer_wheights = np.random.normal(scale = hidden_layer_st_dev, size = (hidden_layer_size, input_layer_size))
-        self.output_layer_wheights = np.random.normal(scale = output_layer_st_dev, size = (output_layer_size, hidden_layer_size))
+        # intialise weights
+        self.hidden_layer_weights = np.random.normal(scale = hidden_layer_st_dev, size = (hidden_layer_size, input_layer_size))
+        self.output_layer_weights = np.random.normal(scale = output_layer_st_dev, size = (output_layer_size, hidden_layer_size))
+        # intialise biases
         self.hidden_layer_biases = np.zeros((hidden_layer_size, 1))
         self.output_layer_biases = np.zeros((output_layer_size, 1))
+    
+    def input_reshape(self, data_set, i):
+        # transform input and target from vectors to matrices with one column for an easier multiplication
+        input_layer = data_set[0][i].reshape(-1, 1) # matrix w/ one column
+        input_target = data_set[1][i]
+        # one hot encoding
+        binary_target = np.array([0 for _ in range(3)])
+        binary_target[input_target] = 1
+        binary_target.shape += (1,)
+        return input_layer, binary_target
 
+    # activation fct & its derivative
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
     
@@ -17,41 +32,33 @@ class Network():
         return x * (1 - x)
     
     def fwd_propagation(self, input_layer):
-        hidden_layer_preactivation = self.hidden_layer_biases +  np.dot(self.hidden_layer_wheights, input_layer)
+        hidden_layer_preactivation = self.hidden_layer_biases +  np.dot(self.hidden_layer_weights, input_layer)
         hidden_layer_activation = self.sigmoid(hidden_layer_preactivation)
 
-        output_layer_preactivation = self.output_layer_biases + np.dot(self.output_layer_wheights, hidden_layer_activation)
+        output_layer_preactivation = self.output_layer_biases + np.dot(self.output_layer_weights, hidden_layer_activation)
         output_layer_activation = self.sigmoid(output_layer_preactivation)
 
         return hidden_layer_activation, output_layer_activation
     
     def backpropagation(self, input_layer, binary_target, hidden_layer_activation, output_layer_activation, learning_rate):
         output_layer_delta = output_layer_activation - binary_target
-        hidden_layer_delta = np.dot(np.transpose(self.output_layer_wheights), output_layer_delta) * self.sigmoid_derivative(hidden_layer_activation)
+        hidden_layer_delta = np.dot(np.transpose(self.output_layer_weights), output_layer_delta) * self.sigmoid_derivative(hidden_layer_activation)
 
-        self.output_layer_wheights -= learning_rate * np.dot(output_layer_delta, np.transpose(hidden_layer_activation))
+        # adjusting weights & biases for output & hidden layer
+        self.output_layer_weights -= learning_rate * np.dot(output_layer_delta, np.transpose(hidden_layer_activation))
         self.output_layer_biases -= learning_rate * output_layer_delta
 
-        self.hidden_layer_wheights -= learning_rate * np.dot(hidden_layer_delta, np.transpose(input_layer))
+        self.hidden_layer_weights -= learning_rate * np.dot(hidden_layer_delta, np.transpose(input_layer))
         self.hidden_layer_biases -= learning_rate * hidden_layer_delta
-
-    def input_reshape(self, data_set, i):
-        # transform input and target from vectors to matrices with one column for an easier multiplication
-        input_layer = data_set[0][i].reshape(-1, 1)
-        input_target = data_set[1][i]
-        binary_target = np.array([0 for x in range(3)])
-        binary_target[input_target] = 1
-        binary_target.shape += (1,)
-        return input_layer, binary_target
     
     def test(self, data_set):
-        size = len(data_set[0])
+        size = len(data_set[0]) # dataset size
         correct_count = 0
-        actual = []
-        predicted = []
-        input_results = {}
+        actual = [] # actual class labels
+        predicted = [] # predicted class labels
+        input_results = {} # input results vs correct results
         for i in range(size):
-            input_layer, binary_target = self.input_reshape(data_set, i)
+            input_layer, binary_target = self.input_reshape(data_set, i) # input and target data
 
             # fwd propagation
             _, output_layer_activation = self.fwd_propagation(input_layer)
@@ -62,7 +69,8 @@ class Network():
             actual += [np.argmax(output_layer_activation)]
             predicted += [np.argmax(binary_target)]
 
-        print(f"Test Accuracy: {round((correct_count / size) * 100, 2)}%")
+        print(f"test accuracy: {round((correct_count / size) * 100, 2)}%")
+        # actual labels, predicted labels, input correctness result
         return (np.array(actual), np.array(predicted)), input_results
 
     def train(self, data_set, learning_rate, epochs_count):
@@ -76,5 +84,5 @@ class Network():
                 correct_count += int(np.argmax(output_layer_activation) == np.argmax(binary_target))
                 self.backpropagation(input_layer, binary_target, hidden_layer_activation, output_layer_activation, learning_rate)
 
-            print(f"Training Accuracy (epoch {epoch}): {round((correct_count / size) * 100, 2)}%")
+            print(f"training accuracy (epoch {epoch}): {round((correct_count / size) * 100, 2)}%")
             correct_count = 0
